172829b5269f90dc64cfae74e549a22d2da3eca7
evanmaxsalmina <ytrie09@gmail.com>
2024-09-22 19:26:53 +0700

    Profile

--- lib/pages/menu/profile.dart
+++ lib/pages/menu/profile.dart
@@ -2,21 +2,75 @@ import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
-      ),
+      backgroundColor: Color.fromARGB(255, 28, 21, 36),
       body: Center(
-        child: Container(
-          child: Column(
-            children: [
-              Text("INI PROFILE"),
-              
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Profile picture
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+            ),
+            SizedBox(height: 20),
+            // Username
+            Text(
+              'Username : Dapara',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            SizedBox(height: 10),
+            // Bio
+            Text(
+              'Bio : YUNG IYUNG IYUNG',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            SizedBox(height: 20),
+            // Watchlist
+            Text(
+              'Watchlist:',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            SizedBox(height: 10),
+            // Watchlist items
+            GridView.count(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              shrinkWrap: true,
+              children: <Widget>[
+                // Watchlist item 1
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      // Image.network('https://via.placeholder.com/150'),
+                      Text('Movie 1'),
+                    ],
+                  ),
+                ),
+                // Watchlist item 2
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      // Image.network('https://via.placeholder.com/150'),
+                      Text('Movie 2'),
+                    ],
+                  ),
+                ),
+                // Watchlist item 3
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      // Image.network('https://via.placeholder.com/150'),
+                      Text('Movie 3'),
+                    ],
+                  ),
+                ),
+                // Add more watchlist items here
+              ],
+            ),
+          ],
         ),
       ),
     );
